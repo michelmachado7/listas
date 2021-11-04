8225692826df6a08b89edc432a7ef18235d1693d
Michel <michelmachado11@hotmail.com>
2021-11-04 00:28:24 -0300

    arquivo 1

---
+++ 1.c
@@ -0,0 +1,17 @@
+/*Escreva uma função que imprima a seguite mensagem
+**********************
+*    Parabens para voce  *
+********************** */
+
+#include<stdio.h>
+
+int function(){
+	printf("**********************\n*    Parabens para voce  *\n**********************");
+	
+}
+int main ()
+{
+	function();
+return 0;
+}
+
